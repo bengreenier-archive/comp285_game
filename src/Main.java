92d6ebacfb1de1e79866ecf86aefcbdf1e240ceb
B3nGr33ni3r <mygithub@b3nhub.com>
2012-09-13 10:43:39 -0400

    Some documentation comments added to main
    I wanted to clarify exactly whats happening in main, so i explaned it
    better, through comments.

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,23 @@ public class Main {
 
 	public static void main(String[] args)
 	{
+		//define array of globjects
 		graphical.GlObject[] GlObjects = new graphical.GlObject[2];
-		/*graphical.Vertex[] t = new graphical.Vertex[4];
-		t[0]=new graphical.Vertex(100,100);
-		t[1]=new graphical.Vertex(100+200,100);
-		t[2]=new graphical.Vertex(100+200,100+200);
-		t[3]=new graphical.Vertex(100,100+200);*/
 		
+		//set globjects[0] to a new rectangle shape
 		GlObjects[0] = new shapes.Rectangle(new graphical.Vertex(10,10), 100, 200, new graphical.RGBA(0, 0, 255, 1));
-
+		
+		//define some custom vertices, to craft a 2d square type shape
 		graphical.Vertex[] i = new graphical.Vertex[4];
 		i[0]=new graphical.Vertex(400,20);
 		i[1]=new graphical.Vertex(700,20);
 		i[2]=new graphical.Vertex(700,100);
 		i[3]=new graphical.Vertex(400,100);
 
-
-		//GlObjects[0]=new graphical.GlObject(t,new graphical.RGBA(255,0,0,1));
+		//using those vertices, and a color create a new globject
 		GlObjects[1]=new graphical.GlObject(i,new graphical.RGBA(0,0,255,1));
 
+		//create the rendering loop, and start it running with the globjects.
 		graphical.Drawable displayExample = new graphical.Drawable();
 		displayExample.start(GlObjects);
 	}
