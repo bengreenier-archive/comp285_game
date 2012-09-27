d1547b138a56af979470603824edd9dd6dcb2c2d
B3nGr33ni3r <mygithub@b3nhub.com>
2012-09-27 00:20:39 -0400

    Refactored Main
    Allowing Main instances to spawn, and the main method constructs a Main
    instance, that encapsulates all game elements. This way, static
    variables probably won't be needed. Well, less needed anyway.

--- 2D_Framework/src/core/Main.java
+++ 2D_Framework/src/core/Main.java
@@ -5,98 +5,130 @@ import java.util.ArrayList;
 import org.lwjgl.input.Keyboard;
 
 
-import core.*;
-
 public class Main {
 
 	public static drawing.RenderLoop gameLoop=null;
 	
+	public static Main mainPtr=null;
+	
+	private drawing.RenderLoop renderLoop;
+	private ArrayList<GameObject> objectsList;
+	public String title;
+	
+	public Main()
+	{
+		mainPtr=this;
+		title="";
+		setRenderLoop(null);
+		setObjectsList(null);
+		instantiate();
+	}
+	
+	public void instantiate()
+	{
+		//TODO override this when you create a Main
+		
+	}
+	
 	public static void main(String[] args)
 	{
-			ArrayList<GameObject> objectsList = new ArrayList<GameObject>();//dynamic list example
-			
-		/*	
-			//create a game object, and set some properties on it.
-			GameObject gameobject = new GameObject();
-			gameobject.setColor(new util.RGBA(255, 255, 255, 1));
-			gameobject.setVertices(util.Vertex.createSquare(new util.Vertex(100,100), 100));
-			gameobject.GL_TYPE=drawing.GL_TYPES.QUAD;
-			
-			//note, this a multiline, not so smooth way to do this
-			objectsList.add(gameobject);
-			
-			
-			//note how smooth this is, and easy to implement, without cluttering the "namespace" up with var names.
-			objectsList.add(new GameObject(){
+		//define the main instance, and instantiate it, creating object
+			Main main = new Main(){
 				@Override
 				public void instantiate()
 				{
-					this.setColor(new util.RGBA(255,0,0,1));
-					this.setVertices(util.Vertex.createSquare(new util.Vertex(200,200), 100));
-					this.GL_TYPE=drawing.GL_TYPES.QUAD;
+					title="Core Main Instance";//just to identify Main objects by string
+					//create the mains objectsList and renderLoop
+					setObjectsList(new ArrayList<GameObject>());
+					setRenderLoop(new drawing.RenderLoop());
+					
+					getObjectsList().add(new core.Character(new util.Vertex(20,20),20));
+					//this allows KEY_ESCAPE to quit the game [a non visual object]
+					getObjectsList().add(new GameObject(){
+						@Override
+						public void instantiate()
+						{
+							this.GL_TYPE=drawing.GL_TYPES.NOTHING;
+							this.setEvent(new input.Event(){
+
+								@Override
+								public void mouse() {
+									// TODO Auto-generated method stub
+									
+								}
+
+								@Override
+								public void other() {
+									// TODO Auto-generated method stub
+									
+								}
+
+								@Override
+								public void preKeys() {
+									// TODO Auto-generated method stub
+									
+								}
+
+								@Override
+								public void postKeys() {
+									// TODO Auto-generated method stub
+									
+								}
+
+								@Override
+								public void upKeys() {
+									// TODO Auto-generated method stub
+									
+								}
+
+								@Override
+								public void downKeys() {
+									// TODO Auto-generated method stub
+									if (Keyboard.getEventKey() == Keyboard.KEY_ESCAPE)
+							    		System.exit(0);
+								}});
+						}
+					});
+					
+					getRenderLoop().setGlobjects(getObjectsList());
+					getRenderLoop().start();
 				}
-			});
-			*/
-			objectsList.add(new core.Character(new util.Vertex(20,20),20));
-			
-			
-			//this allows KEY_ESCAPE to quit the game [a non visual object]
-			objectsList.add(new GameObject(){
-				@Override
-				public void instantiate()
-				{
-					this.GL_TYPE=drawing.GL_TYPES.NOTHING;
-					this.setEvent(new input.Event(){
-
-						@Override
-						public void mouse() {
-							// TODO Auto-generated method stub
-							
-						}
-
-						@Override
-						public void other() {
-							// TODO Auto-generated method stub
-							
-						}
-
-						@Override
-						public void preKeys() {
-							// TODO Auto-generated method stub
-							
-						}
-
-						@Override
-						public void postKeys() {
-							// TODO Auto-generated method stub
-							
-						}
-
-						@Override
-						public void upKeys() {
-							// TODO Auto-generated method stub
-							
-						}
-
-						@Override
-						public void downKeys() {
-							// TODO Auto-generated method stub
-							if (Keyboard.getEventKey() == Keyboard.KEY_ESCAPE)
-					    		System.exit(0);
-						}});
-				}
-			});
+			};
 			
 			
 			
-			drawing.RenderLoop loop = new drawing.RenderLoop();
-			gameLoop=loop;
 			
-			loop.setGlobjects(objectsList);
-			loop.start();
-			System.out.println("Yay, Threaded!");
+			System.out.println("Yay, Threaded! "+main.title+" created.");
 			
 		
+	}
+
+	/**
+	 * @return the renderLoop
+	 */
+	public drawing.RenderLoop getRenderLoop() {
+		return renderLoop;
+	}
+
+	/**
+	 * @param renderLoop the renderLoop to set
+	 */
+	public void setRenderLoop(drawing.RenderLoop renderLoop) {
+		this.renderLoop = renderLoop;
+	}
+
+	/**
+	 * @return the objectsList
+	 */
+	public ArrayList<GameObject> getObjectsList() {
+		return objectsList;
+	}
+
+	/**
+	 * @param objectsList the objectsList to set
+	 */
+	public void setObjectsList(ArrayList<GameObject> objectsList) {
+		this.objectsList = objectsList;
 	}
 	
 	
