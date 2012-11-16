7b946957390c891518565c27d4f67c2cb3bd658d
B3nGr33ni3r <mygithub@b3nhub.com>
2012-11-16 00:05:08 -0500

    Basic Ribbon/Dragging Functionality
    can drag "little" towers from ribbon.

--- src/com/bengreenier/smashgrab/states/Build.java
+++ src/com/bengreenier/smashgrab/states/Build.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.GameState;
@@ -12,19 +13,25 @@ import org.newdawn.slick.state.StateBasedGame;
 import com.bengreenier.smashgrab.ribbons.Ribbon;
 import com.bengreenier.smashgrab.ribbons.RibbonItem;
 
+import com.bengreenier.slick.tiling.TileView;
 import com.bengreenier.slick.util.GameObject;
+import com.bengreenier.slick.util.Vector2i;
 
 public class Build implements GameState {
 
 	private ArrayList<GameObject> objects;
 	private Ribbon ribbon;
+	private TileView tv;
+	private Image mouseSprite;
 	private int id;
 	
 	public Build(int id)
 	{
 		this.id = id;
 		objects = new ArrayList<GameObject>();
-		ribbon = new Ribbon("#A60000",802,80,0,700);
+		ribbon = new Ribbon("#3399CC",802,80,0,500);
+		tv = new TileView(16, 12, 50, 50);
+		mouseSprite=null;
 	}
 	
 	@Override
@@ -46,13 +53,155 @@ public class Build implements GameState {
 	public void init(GameContainer arg0, StateBasedGame arg1)
 			throws SlickException {
 		
+		final Build localFinalBuildPointer = this;
+		
+		arg0.getInput().addListener(ribbon);
+		
+		
+		
 		ribbon.addRibbonItem(new RibbonItem(){
+			
+			private Vector2i start;
+			
+			
 			@Override
-			public void mouseDragged(int arg0, int arg1, int arg2, int arg3) {
-				System.out.println("Dragged from "+arg0+","+arg1+" "+arg2+","+arg3);
+			public void instantiate()
+			{
+				try {
+					addImageAndResize(new Image("res/MachineGunTower.png"));
+				} catch (SlickException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
+			
+			@Override
+			public void mousePressed(int arg0, int arg1, int arg2)
+			{
+				if (arg0 == Input.MOUSE_LEFT_BUTTON && inBounds(arg1,arg2))
+					{
+						start = new Vector2i(arg1,arg2);
+						//bad practice, creating an Image here
+						Image t=null;
+						try {
+							t = new Image("res/MachineGunTower.png").getScaledCopy(.5f);
+						} catch (SlickException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						if (t!=null)
+						localFinalBuildPointer.mouseSprite = t;
+					}
+			}
+			
+			@Override
+			public void mouseReleased(int arg0, int arg1, int arg2)
+			{
+				if (arg0 == Input.MOUSE_LEFT_BUTTON && start!=null)
+				{
+					System.out.println("you moved from "+start+" to "+new Vector2i(arg1,arg2));
+					localFinalBuildPointer.mouseSprite=null;
+				}
+			}
+			
 		});
 		
+		ribbon.addRibbonItem(new RibbonItem(){
+			
+			private Vector2i start;
+			
+			
+			@Override
+			public void instantiate()
+			{
+				try {
+					addImageAndResize(new Image("res/RocketTower.png"));
+				} catch (SlickException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			@Override
+			public void mousePressed(int arg0, int arg1, int arg2)
+			{
+				if (arg0 == Input.MOUSE_LEFT_BUTTON && inBounds(arg1,arg2))
+					{
+						start = new Vector2i(arg1,arg2);
+						//bad practice, creating an Image here
+						Image t=null;
+						try {
+							t = new Image("res/RocketTower.png").getScaledCopy(.5f);
+						} catch (SlickException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						if (t!=null)
+						localFinalBuildPointer.mouseSprite = t;
+					}
+			}
+			
+			@Override
+			public void mouseReleased(int arg0, int arg1, int arg2)
+			{
+				if (arg0 == Input.MOUSE_LEFT_BUTTON && start!=null)
+				{
+					System.out.println("you moved from "+start+" to "+new Vector2i(arg1,arg2));
+					localFinalBuildPointer.mouseSprite = null;
+				}
+			}
+			
+		});
+
+		ribbon.addRibbonItem(new RibbonItem(){
+			
+			private Vector2i start;
+			
+			
+			@Override
+			public void instantiate()
+			{
+				try {
+					addImageAndResize(new Image("res/LaserTower.png"));
+				} catch (SlickException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			@Override
+			public void mousePressed(int arg0, int arg1, int arg2)
+			{
+				if (arg0 == Input.MOUSE_LEFT_BUTTON && inBounds(arg1,arg2))
+					{
+						start = new Vector2i(arg1,arg2);
+						//bad practice, creating an Image here
+						Image t=null;
+						try {
+							t = new Image("res/LaserTower.png").getScaledCopy(.5f);
+						} catch (SlickException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						if (t!=null)
+						localFinalBuildPointer.mouseSprite = t;
+					}
+			}
+			
+			@Override
+			public void mouseReleased(int arg0, int arg1, int arg2)
+			{
+				if (arg0 == Input.MOUSE_LEFT_BUTTON && start!=null)
+				{
+					System.out.println("you moved from "+start+" to "+new Vector2i(arg1,arg2));
+					localFinalBuildPointer.mouseSprite = null;
+				}
+			}
+			
+		});
 		
 	}
 
@@ -68,9 +217,20 @@ public class Build implements GameState {
 			throws SlickException {
 		if (ribbon!=null)
 			ribbon.draw(arg2);
+		else
+			try{throw new Exception("Ribbon is null");}catch(Exception e){e.printStackTrace();}
 		
+		/*if (tv != null)
+			tv.debugDraw(arg0, arg2);
+		else
+			try{throw new Exception("TileView is null");}catch(Exception e){e.printStackTrace();}
+		*/
 		for (GameObject o : objects)
 			o.render(arg0, arg2);
+		
+		
+		if (mouseSprite!=null)
+			mouseSprite.draw(arg0.getInput().getMouseX(),arg0.getInput().getMouseY());
 		
 	}
 
