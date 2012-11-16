67d713a24b288897da49b51e26033c2db4417173
B3nGr33ni3r <mygithub@b3nhub.com>
2012-11-16 00:03:31 -0500

    Main Configured to test Build only

--- src/com/bengreenier/smashgrab/main/Main.java
+++ src/com/bengreenier/smashgrab/main/Main.java
@@ -2,41 +2,21 @@ package com.bengreenier.smashgrab.main;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
-import com.bengreenier.slick.messages.Message;
-import com.bengreenier.slick.messages.MessageSystem;
-import com.bengreenier.slick.tiling.Tile;
-import com.bengreenier.slick.tiling.TileView;
-import com.bengreenier.slick.util.StateFreeGame;
-import com.bengreenier.slick.util.Vector2i;
-import com.bengreenier.smashgrab.enemies.BlueBlob;
+import com.bengreenier.smashgrab.states.Build;
 import com.bengreenier.smashgrab.states.MainMenu;
 import com.bengreenier.smashgrab.states.Paused;
-import com.bengreenier.smashgrab.towers.MachineGunTower;
 
 public class Main {
 
 	public static Main core;
 	public static void main(String[] args) throws SlickException { core = new Main(); }
-	
-	@Deprecated
-	public static final int TILESIZE_X=50;
-	@Deprecated
-	public static final int TILESIZE_Y=50;
-
-	@Deprecated
-	public enum Mode{BUILDER,VIEWER};
-	@Deprecated
-	private enum BUILDER{MACH};
 
 	public AppGameContainer agc;
 	public StateBasedGame sfg;
-	public Mode mode;
-
+	
 	//store our static int id's here
 	public static class ID{
 		public static int PAUSED = 0;
@@ -51,9 +31,10 @@ public class Main {
 
 			@Override
 			public void initStatesList(GameContainer arg0) throws SlickException {
-				addState(new Paused(ID.PAUSED));
-				addState(new MainMenu(ID.MAINMENU));
-				enterState(ID.PAUSED);
+				//addState(new Paused(ID.PAUSED));
+				//addState(new MainMenu(ID.MAINMENU));
+				addState(new Build(ID.BUILD));
+				enterState(ID.BUILD);
 				
 			}
 			
