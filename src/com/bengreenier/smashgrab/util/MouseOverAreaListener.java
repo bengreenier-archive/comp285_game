ac9d5c92041c6180b3161ee4fa2fe4ffc3589cca
Joe Gualtieri <xblink182_jlgx@yahoo.com>
2012-11-15 21:27:18 -0500

    removed unneeded stuff

--- src/com/bengreenier/smashgrab/util/MouseOverAreaListener.java
+++
@@ -1,7 +0,0 @@
-package com.bengreenier.smashgrab.util;
-
-import org.newdawn.slick.GameContainer;
-
-public interface MouseOverAreaListener {
-	public void onNotify(GameContainer gc, MouseOverArea mouseOvrArea);
-}
