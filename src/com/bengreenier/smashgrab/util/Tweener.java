39a9d25022ccce1d3b92958b799275e287c266dd
B3nGr33ni3r <mygithub@b3nhub.com>
2012-11-19 12:49:45 -0500

    Began Tweener work / tests

--- src/com/bengreenier/smashgrab/util/Tweener.java
+++ src/com/bengreenier/smashgrab/util/Tweener.java
@@ -3,16 +3,17 @@ package com.bengreenier.smashgrab.util;
 import com.bengreenier.slick.util.Vector2i;
 
 /**
- * takes a Vector2i position, a Vector2i point, and a time in ms
+ * takes a Vector2i position, a Vector2i point, and a total time
  * and then calling .getNextPoint will smooth return the position for
  * a move for the GameObject to the point over the time.
  * @author B3N
  * @version 0.1
  */
 public class Tweener {
-
+	
 	private Vector2i pos,point;
-	int time;
+	private int time;
+	private int rtot=0;
 	
 	public Tweener(Vector2i pos, Vector2i point, int time)
 	{
@@ -28,6 +29,22 @@ public class Tweener {
 	 */
 	public Vector2i getNextPoint(int delta)
 	{
-		return new Vector2i();
+		rtot+=delta;
+		//pos = rtot/time of Vector2i.subtract(point,pos); 
+		Vector2i sub = Vector2i.subtract(point, pos);
+		pos = new Vector2i(sub.getX()*(time/rtot),sub.getY()*(time/rtot));
+		
+		return pos;
+	}
+	
+	public static void main(String args[])
+	{
+		Tweener t = new Tweener(new Vector2i(0,0),new Vector2i(50,0),2000);
+		Vector2i pt = t.getNextPoint(5);
+		while(pt.getX() != 50)
+		{
+			System.out.println(pt);
+			pt = t.getNextPoint(5);
+		}
 	}
 }
